36cb17c7d2bccd2bcb7d0b622a29a9a7c969aa51
aashish2998 <97464622+aashish2998@users.noreply.github.com>
2022-01-28 23:42:32 +0530

    Create find an element in array.cpp

---
+++ find an element in array.cpp
@@ -0,0 +1,29 @@
+### find an element in array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+ 
+   int N,K;
+   cin>>N;
+   cin>>K;
+   int arr[N];
+   int i;
+    for(  i=0;i<N;i++)
+    {
+        cin>>arr[i];
+        
+        if(arr[i]==K)
+    {
+        cout<<1;
+        break;
+    }
+        if(i==N-1)
+   {
+       cout<<-1;
+   }
+    }
+	return 0;
+
+}
